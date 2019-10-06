83fd55b8d7afc24b97d42e3831716394356ddbae
Willian Dias Tamagi <will.tamagi@gmail.com>
2019-10-05 22:29:54 -0300

    Added BrowserRouter component to wrapper all components.

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+import { BrowserRouter } from 'react-router-dom'
+
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+  uri: 'http://localhost:4000',
 })
 
 const client = new ApolloClient({
@@ -19,10 +21,13 @@ const client = new ApolloClient({
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
-, document.getElementById('root'))
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
