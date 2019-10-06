fdf17bdefa4a0c484f976bc41bdde5f1a1c81ab0
Willian Dias Tamagi <will.tamagi@gmail.com>
2019-10-05 22:28:34 -0300

    Added Header component to main component and use route to diplay the components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
 import '../styles/App.css'
 import LinkList from './LinkList'
+import Header from './Header'
+import CreateLink from './CreateLink'
 
-const App = props => <LinkList />
+const App = props => (
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Switch>
+        <Route exact path="/" component={LinkList} />
+        <Route exact path="/create" component={CreateLink} />
+      </Switch>
+    </div>
+  </div>
+)
 
 export default App
