8bf4ae5bbdbbbd40f47bd2de9304e15420221777
Willian Dias Tamagi <will.tamagi@gmail.com>
2019-10-22 10:52:44 -0300

    Added cache update when store new link.

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 
+import { FEED_QUERY } from './LinkList'
+
 const POST_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
     post(description: $description, url: $url) {
@@ -43,6 +45,14 @@ class CreateLink extends Component {
           mutation={POST_MUTATION}
           variables={{ description, url }}
           onCompleted={() => this.props.history.push('/')}
+          update={(store, { data: { post } }) => {
+            const data = store.readQuery({ query: FEED_QUERY })
+            data.feed.links.unshift(post)
+            store.writeQuery({
+              query: FEED_QUERY,
+              data,
+            })
+          }}
         >
           {postMutation => <button onClick={postMutation}>Submit</button>}
         </Mutation>
