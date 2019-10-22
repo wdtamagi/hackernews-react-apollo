ca9b9ab55d036b27c5cd1d63b5d91b44e08e4fc4
Willian Dias Tamagi <will.tamagi@gmail.com>
2019-10-22 14:59:33 -0300

    Added subscription query and update store when received a new link creation.

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -27,6 +27,27 @@ export const FEED_QUERY = gql`
   }
 `
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
+
 const LinkList = () => {
   const _updateCacheAfterVote = (store, createVote, linkId) => {
     const data = store.readQuery({ query: FEED_QUERY })
@@ -37,11 +58,33 @@ const LinkList = () => {
     store.writeQuery({ query: FEED_QUERY, data })
   }
 
+  const _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev
+        const newLink = subscriptionData.data.newLink
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id)
+        if (exists) return prev
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [...prev.feed.links, newLink],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename,
+          },
+        })
+      },
+    })
+  }
+
   return (
     <Query query={FEED_QUERY}>
-      {({ loading, error, data }) => {
+      {({ loading, error, data, subscribeToMore }) => {
         if (loading) return <div>Fetching</div>
         if (error) return <div>Error</div>
+
+        _subscribeToNewLinks(subscribeToMore)
 
         const linksToRender = data.feed.links
 
