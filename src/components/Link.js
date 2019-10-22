4006a962a5c6b7d618ca7c001452e09415419502
Willian Dias Tamagi <will.tamagi@gmail.com>
2019-10-22 15:08:06 -0300

    Added subscription to new vote, and removed update function for upvote mutation.

--- src/components/Link.js
+++ src/components/Link.js
@@ -27,36 +27,33 @@ const VOTE_MUTATION = gql`
 const Link = props => {
   const authToken = localStorage.getItem(AUTH_TOKEN)
   return (
-    <Mutation
-      mutation={VOTE_MUTATION}
-      variables={{ linkId: props.link.id }}
-      update={(store, { data: { vote } }) =>
-        props.updateStoreAfterVote(store, vote, props.link.id)
-      }
-    >
-      {voteMutation => (
-        <div className="flex mt2 items-start">
-          <div className="flex items-center">
-            <span className="gray">{props.index + 1}.</span>
-            {authToken && (
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{props.index + 1}.</span>
+        {authToken && (
+          <Mutation
+            mutation={VOTE_MUTATION}
+            variables={{ linkId: props.link.id }}
+          >
+            {voteMutation => (
               <div className="ml1 gray f11" onClick={voteMutation}>
                 ▲
               </div>
             )}
-          </div>
-          <div className="ml1">
-            <div>
-              {props.link.description} ({props.link.url})
-            </div>
-            <div className="f6 lh-copy gray">
-              {props.link.votes.length} votes | by{' '}
-              {props.link.postedBy ? props.link.postedBy.name : 'Unknown'}{' '}
-              {timeDifferenceForDate(props.link.createdAt)}
-            </div>
-          </div>
+          </Mutation>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {props.link.description} ({props.link.url})
         </div>
-      )}
-    </Mutation>
+        <div className="f6 lh-copy gray">
+          {props.link.votes.length} votes | by{' '}
+          {props.link.postedBy ? props.link.postedBy.name : 'Unknown'}{' '}
+          {timeDifferenceForDate(props.link.createdAt)}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -48,16 +48,34 @@ const NEW_LINKS_SUBSCRIPTION = gql`
   }
 `
 
-const LinkList = () => {
-  const _updateCacheAfterVote = (store, createVote, linkId) => {
-    const data = store.readQuery({ query: FEED_QUERY })
-
-    const votedLink = data.feed.links.find(link => link.id === linkId)
-    votedLink.votes = createVote.link.votes
-
-    store.writeQuery({ query: FEED_QUERY, data })
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
   }
+`
 
+const LinkList = () => {
   const _subscribeToNewLinks = subscribeToMore => {
     subscribeToMore({
       document: NEW_LINKS_SUBSCRIPTION,
@@ -78,6 +96,12 @@ const LinkList = () => {
     })
   }
 
+  const _subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION,
+    })
+  }
+
   return (
     <Query query={FEED_QUERY}>
       {({ loading, error, data, subscribeToMore }) => {
@@ -85,18 +109,14 @@ const LinkList = () => {
         if (error) return <div>Error</div>
 
         _subscribeToNewLinks(subscribeToMore)
+        _subscribeToNewVotes(subscribeToMore)
 
         const linksToRender = data.feed.links
 
         return (
           <div>
             {linksToRender.map((link, index) => (
-              <Link
-                key={link.id}
-                link={link}
-                index={index}
-                updateStoreAfterVote={_updateCacheAfterVote}
-              />
+              <Link key={link.id} link={link} index={index} />
             ))}
           </div>
         )
