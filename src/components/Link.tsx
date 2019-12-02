cbd7a0065d6f886106cb15bad5a92a64776c70cf
Willian Dias Tamagi <will.tamagi@gmail.com>
2019-12-01 21:39:08 -0300

    Added converted typescript files.

---
+++ src/components/Link.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import { AUTH_TOKEN } from '../constants'
+import { timeDifferenceForDate } from '../utils'
+import { useMutation } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
+
+const Link = ({ index, link }: any) => {
+  const authToken = localStorage.getItem(AUTH_TOKEN)
+
+  const [voteMutation] = useMutation(VOTE_MUTATION)
+
+  return (
+    <div className='flex mt2 items-start'>
+      <div className='flex items-center'>
+        <span className='gray'>{index + 1}.</span>
+        {authToken && (
+          <div
+            className='ml1 gray f11'
+            onClick={() => voteMutation({ variables: { linkId: link.id } })}
+          >
+            ▲
+          </div>
+        )}
+      </div>
+      <div className='ml1'>
+        <div>
+          {link.description} ({link.url})
+        </div>
+        <div className='f6 lh-copy gray'>
+          {link.votes.length} votes | by{' '}
+          {link.postedBy ? link.postedBy.name : 'Unknown'}{' '}
+          {timeDifferenceForDate(link.createdAt)}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Link
---
+++ src/components/Login.tsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react'
+import { AUTH_TOKEN } from '../constants'
+import { useMutation } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`
+
+const Login = ({ history }: any) => {
+  const [login, setLogin] = useState(true)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+
+  const _confirm = async (data: any) => {
+    const { token } = login ? data.login : data.signup
+    _saveUserData(token)
+    history.push(`/`)
+  }
+
+  const _saveUserData = (token: any) => {
+    localStorage.setItem(AUTH_TOKEN, token)
+  }
+
+  const [signupMutation] = useMutation(SIGNUP_MUTATION, {
+    onCompleted: data => _confirm(data),
+  })
+  const [loginMutation] = useMutation(LOGIN_MUTATION, {
+    onCompleted: data => _confirm(data),
+  })
+
+  return (
+    <div>
+      <h4 className='mv3'>{login ? 'Login' : 'Sign Up'}</h4>
+      <div className='flex flex-column'>
+        {!login && (
+          <input
+            value={name}
+            onChange={e => setName(e.target.value)}
+            type='text'
+            placeholder='Your name'
+          />
+        )}
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          type='text'
+          placeholder='Your email address'
+        />
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type='password'
+          placeholder='Choose a safe password'
+        />
+      </div>
+      <div className='flex mt3'>
+        <div
+          className='pointer mr2 button'
+          onClick={() => {
+            if (login) {
+              loginMutation({ variables: { email, password, name } })
+            } else {
+              signupMutation({ variables: { email, password, name } })
+            }
+          }}
+        >
+          {login ? 'login' : 'create account'}
+        </div>
+
+        {/* <Mutation
+          mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
+          variables={{ email, password, name }}
+          onCompleted={data => _confirm(data)}
+        >
+          {mutation => (
+            <div className="pointer mr2 button" onClick={mutation}>
+              {login ? 'login' : 'create account'}
+            </div>
+          )}
+        </Mutation> */}
+        <div
+          className='pointer button'
+          onClick={() => setLogin(currentLogin => !currentLogin)}
+        >
+          {login ? 'need to create an account?' : 'already have an account?'}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
